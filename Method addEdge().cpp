98c446d49a9d14c42717a1a03129ef4e1dd6a184
BDDimitrov18 <58163537+BDDimitrov18@users.noreply.github.com>
2021-12-01 09:23:34 +0200

    gogo

--- Method addEdge().cpp
+++ Method addEdge().cpp
@@ -1,13 +1,12 @@
-//Method to define an undirected graph
 void Graph::addEdge(int source, int destination)
 {
-  adjacencyLists[source].push_back(destination);
+    adjacencyLists[source].push_back(destination);
 
-  adjacencyLists[destination].push_back(source);
+    adjacencyLists[destination].push_back(source);
 }
 
 //Method to define an directed graph
-void Graph::addEdge(int source, int destination)
+/*void Graph::addEdge(int source, int destination)
 {
-  adjacencyLists[source].push_back(destination);
-}
+    adjacencyLists[source].push_back(destination);
+}*/
