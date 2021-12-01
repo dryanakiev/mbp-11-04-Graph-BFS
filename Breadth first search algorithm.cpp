3e54988771f558e4dbd8e1d12037a46115d60d2e
kvbolashikova18 <59692632+kvbolashikova18@users.noreply.github.com>
2021-12-01 19:56:56 +0200

    Add missing code

--- Breadth first search algorithm.cpp
+++ Breadth first search algorithm.cpp
@@ -3,22 +3,29 @@ void Graph::BFS(int startingVertex)
   visited = new bool[numberOfVertices];
 
   //Initialize visited pointer with false
+  for (int i = 0; i < numberOfVertices; i++)
+  {
+      visited[i] = false;
+  }
 
-  list<int> queue;
+  std::list<int> queue;
 
   visited[startingVertex] = true;
 
   //Add the starting vertex to the queue
+  queue.push_back(startingVertex);
 
   //define iterator of type list<int>
+  std::list<int>::iterator listIterator;
 
   while (!queue.empty()) 
   {
     int currentVertex = queue.front();
-    cout << "Visited V" << currentVertex << " ";
+    std::cout << "Visited V" << currentVertex << " ";
     queue.pop_front();
 
     //For loop definition starting from current vertex to last vertex
+    for (auto i = adjacencyLists[currentVertex].begin(); i != adjacencyLists[currentVertex].end(); ++listIterator)
     {
       int adjacentVertex = *listIterator;
 
