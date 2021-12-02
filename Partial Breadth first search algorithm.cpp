e4bea680717a393ee291164d544938c056a4d5c9
AKPatrashkov18 <AKPatrashkov18@codingburgas.bg>
2021-12-02 09:13:29 +0200

    add commit

--- Partial Breadth first search algorithm.cpp
+++ Partial Breadth first search algorithm.cpp
@@ -1,33 +1,56 @@
-void Graph::BFS(int startingVertex) 
+#include<iostream>
+#include<list>
+using namespace std;
+void Graph::BFS(int startingVertex)
 {
-  visited = new bool[numberOfVertices];
+    visited = new bool[numberOfVertices];
 
-  //Initialize visited pointer with false
-
-  list<int> queue;
-
-  visited[startingVertex] = true;
-
-  //Add the starting vertex to the queue
-
-  //define iterator of type list<int>
-
-  while (!queue.empty()) 
-  {
-    int currentVertex = queue.front();
-    cout << "Visited V" << currentVertex << " ";
-    queue.pop_front();
-
-    //For loop definition starting from current vertex to last vertex
+    //Initialize visited pointer with false
+    for (int i = 0; i < numberOfVertices; i++)
     {
-      int adjacentVertex = *listIterator;
-
-      if (!visited[adjacentVertex]) {
-
-        visited[adjacentVertex] = true;
-
-        queue.push_back(adjacentVertex);
-      }
+        visited[i] = false;
     }
-  }
+
+    list<int> queue;
+
+    visited[startingVertex] = true;
+
+    //Add the starting vertex to the queue
+    queue.push_back(s);
+
+    //define iterator of type list<int>
+    list<int>::iterator i;
+
+    while (!queue.empty())
+    {
+        int currentVertex = queue.front();
+        cout << "Visited V" << currentVertex << " ";
+        queue.pop_front();
+
+        //For loop definition starting from current vertex to last vertex
+        for (i = adjacencyLists[s].begin(); i != adjacencyLists[s].end(); ++i)
+        {
+            int adjacentVertex = *listIterator;
+
+            if (!visited[adjacentVertex]) {
+
+                visited[adjacentVertex] = true;
+
+                queue.push_back(adjacentVertex);
+            }
+        }
+    }
+}
+
+void Graph::printGraph()
+{
+    for (int i = 0; i < numberOfVertices; i++)
+    {
+        cout << "Vertex " << i << ":";
+        for (auto x : adjacencyLists[i])
+        {
+            cout << "-> " << x;
+        }
+        cout << endl;
+    }
 }
